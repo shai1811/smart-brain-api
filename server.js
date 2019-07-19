c8b8b712f9c76fdadaeab8a334c9a25ad9dce89d
shai don <shai1811@gmail.com>
2019-07-19 20:43:53 +0300

    adding starting project

---
+++ server.js
@@ -0,0 +1,65 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const bcrypt = require('bcrypt-nodejs');
+const cors = require('cors');
+const knex = require('knex');
+
+const register = require('./controllers/register');
+const signin = require('./controllers/signin');
+const profile = require('./controllers/profile');
+const image = require('./controllers/image');
+
+const db = knex({
+  client: 'pg', //the name of our sql server
+  connection: {
+    host : '127.0.0.1',
+    user : 'postgres',
+    password : 'omer1908',
+    database : 'smartbrain'
+  }
+});
+
+db.select('*').from('users').then(data => {
+	console.log(data);
+});
+
+const app = express();
+
+app.use(bodyParser.json());
+app.use(cors())
+
+app.get('/', (req, res) => {
+	res.send(database.users);
+})
+
+app.post('/signin', (req, res) => {signin.handleSignin(req, res, db, bcrypt)})
+app.post('/register', (req, res) => {register.handleRegister(req, res, db, bcrypt)})
+app.get('/profile/:id', (req, res) => {profile.handleProfileGet(req, res, db)})
+app.put('/image', (req,res) => {image.handleImage(req, res, db)})//increment will add 1 to entries every tine will make pic scan
+app.post('/imageurl', (req,res) => {image.handleApiCall(req, res)})
+
+// bcrypt.hash("bacon", null, null, function(err, hash) {
+//     // Store hash in your password DB.
+// });
+
+// // Load hash from your password DB.
+// bcrypt.compare("bacon", hash, function(err, res) {
+//     // res == true
+// });
+// bcrypt.compare("veggies", hash, function(err, res) {
+//     // res = false
+// });
+
+app.listen(3000, () => {
+	console.log('app is running on port 3000');
+})
+
+
+/*
+/ --> res = this is working
+/ signin --> POST = success/fail
+/ register --> POST = user
+/ profile/:userId --> GET = user
+/ image --> PUT --> user
+
+*/
